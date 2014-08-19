1f280fc2c5606e37d8f99388f9c1faa4574648c9
Peter <peterhsaxton@gmail.com>
2014-08-19 22:29:30 +0100

    first working to execution version

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var shell = require('shelljs')
+
 function getConfigXML () {
     if (fs.existsSync('app/config.xml')) {
         return process.cwd() + '/app/config.xml';
@@ -8,6 +11,8 @@ module.exports = function () {
     var CONFIG_PATH = getConfigXML();
 
     require('./lib/readConfig')(CONFIG_PATH, function (ress) {
-        console.log(ress)
+        var commands = require('./lib/inkscapeCommands')(ress, function (command) {
+            console.log(shell.exec(command));
+        });
     });
 };
